454a413f892b2d09afc5144a044de5b6100d0382
Anner Escobar <anner123escobar@gmail.com>
2025-05-06 22:16:33 -0600

    cambio en el cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://proagua-frontend.netlify.app',
+    credentials: true,
+  });
+  
 
   app.useGlobalPipes(
     new ValidationPipe({
