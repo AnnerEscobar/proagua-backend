a3105b2cee1d9c3471beb1cf7544dd6f64032dd6
Anner Escobar <anner123escobar@gmail.com>
2025-05-06 22:46:30 -0600

    modificando url para cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://proagua-frontend.netlify.app'],
+    origin: ['http://localhost:4200', 'https://proaguaproyect.netlify.app'],
     credentials: true,
   });
   
